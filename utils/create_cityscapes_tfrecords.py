4afc08a57512c05f3f7cc7b46106ba6543156581
Till Beemelmanns <till.beemelmanns@rwth-aachen.de>
2021-02-25 15:47:54 +0100

    use proper val and train dataset for cityscapes

--- utils/create_cityscapes_tfrecords.py
+++ utils/create_cityscapes_tfrecords.py
@@ -54,132 +54,134 @@ CLASS_MAP = {
 def create_records():
     max_objects = cfg['max_objects']
 
-    img_dir = os.path.join(cfg['in_dir'], 'leftImg8bit', cfg['dataset'])
-    label_dir = os.path.join(cfg['in_dir'], 'gtBbox3d', cfg['dataset'])
+    for dataset in cfg['datasets']:
 
-    img_files = glob.glob(os.path.join(img_dir, '*/*.png'))
-    img_files = sorted(img_files)
+        img_dir = os.path.join(cfg['in_dir'], 'leftImg8bit', dataset)
+        label_dir = os.path.join(cfg['in_dir'], 'gtBbox3d', dataset)
 
-    label_files = glob.glob(os.path.join(label_dir, '*/*.json'))
-    label_files = sorted(label_files)
+        img_files = glob.glob(os.path.join(img_dir, '*/*.png'))
+        img_files = sorted(img_files)
 
-    n_scenes = cfg['n_scenes'] if cfg['n_scenes'] > 0 else len(img_files)
-    bar = helpers.progbar(n_scenes)
-    bar.start()
+        label_files = glob.glob(os.path.join(label_dir, '*/*.json'))
+        label_files = sorted(label_files)
 
-    with tf.io.TFRecordWriter(cfg['out_train']) as train_writer, tf.io.TFRecordWriter(cfg['out_val']) as val_writer:
+        n_scenes = cfg['n_scenes'] if cfg['n_scenes'] > 0 else len(img_files)
+        bar = helpers.progbar(n_scenes)
+        bar.start()
 
-        for scene_id, (img_file, label_file) in enumerate(zip(img_files, label_files)):
+        with tf.io.TFRecordWriter(cfg['out_train']) as train_writer, tf.io.TFRecordWriter(cfg['out_val']) as val_writer:
 
-            if scene_id == n_scenes: break
+            for scene_id, (img_file, label_file) in enumerate(zip(img_files, label_files)):
 
-            bar.update(scene_id)
+                if scene_id == n_scenes: break
 
-            assert os.path.basename(img_file)[:-16] == os.path.basename(label_file)[:-14]
+                bar.update(scene_id)
 
-            img_arr = cv.imread(img_file)
-            orig_img_size = img_arr.shape[:2]
-            img_arr = cv.resize(img_arr, (cfg['img_size'][1], cfg['img_size'][0]), interpolation=cv.INTER_CUBIC)
-            _, img = cv.imencode('.png', img_arr)
-            img = img.tobytes()
+                assert os.path.basename(img_file)[:-16] == os.path.basename(label_file)[:-14]
 
-            with open(label_file) as json_file:
-                label_dict = json.load(json_file)
+                img_arr = cv.imread(img_file)
+                orig_img_size = img_arr.shape[:2]
+                img_arr = cv.resize(img_arr, (cfg['img_size'][1], cfg['img_size'][0]), interpolation=cv.INTER_CUBIC)
+                _, img = cv.imencode('.png', img_arr)
+                img = img.tobytes()
 
-            if len(label_dict['objects']) == 0: continue
+                with open(label_file) as json_file:
+                    label_dict = json.load(json_file)
 
-            camera = box_utils.Camera(fx=label_dict['sensor']['fx'],
-                                      fy=label_dict['sensor']['fy'],
-                                      u0=label_dict['sensor']['u0'],
-                                      v0=label_dict['sensor']['v0'],
-                                      sensor_T_ISO_8855=label_dict['sensor']['sensor_T_ISO_8855'])
+                if len(label_dict['objects']) == 0: continue
 
-            K_matrix = np.zeros((4, 4))
-            K_matrix[0][0] = label_dict['sensor']['fx']
-            K_matrix[0][2] = label_dict['sensor']['u0']
-            K_matrix[1][1] = label_dict['sensor']['fy']
-            K_matrix[1][2] = label_dict['sensor']['v0']
-            K_matrix[2][2] = 1
+                camera = box_utils.Camera(fx=label_dict['sensor']['fx'],
+                                          fy=label_dict['sensor']['fy'],
+                                          u0=label_dict['sensor']['u0'],
+                                          v0=label_dict['sensor']['v0'],
+                                          sensor_T_ISO_8855=label_dict['sensor']['sensor_T_ISO_8855'])
 
-            label = {}
-            label['calib'] = K_matrix
-            label['orig'] = np.array(orig_img_size).astype(np.float32)
+                K_matrix = np.zeros((4, 4))
+                K_matrix[0][0] = label_dict['sensor']['fx']
+                K_matrix[0][2] = label_dict['sensor']['u0']
+                K_matrix[1][1] = label_dict['sensor']['fy']
+                K_matrix[1][2] = label_dict['sensor']['v0']
+                K_matrix[2][2] = 1
 
-            label['clf'] = np.ones((max_objects, 1)) * 8
-            label['c_3d'] = np.zeros((max_objects, 3))
-            label['extent'] = np.zeros((max_objects, 3))
-            label['bbox_3d'] = np.zeros((max_objects, 8, 3))
-            label['bbox_2d'] = np.zeros((max_objects, 4))
-            label['c_2d'] = np.zeros((max_objects, 2))
-            label['ri'] = np.zeros((max_objects, 1))
-            label['rj'] = np.zeros((max_objects, 1))
-            for idx, obj in enumerate(label_dict['objects'][:max_objects]):
-                bbox = box_utils.Box3dImageTransform(camera)
-                bbox.initialize_box(center=obj['3d']['center'],
-                                    quaternion=obj['3d']['rotation'],
-                                    size=obj['3d']['dimensions'])
+                label = {}
+                label['calib'] = K_matrix
+                label['orig'] = np.array(orig_img_size).astype(np.float32)
 
-                _, center_3d_cam, quaternion = bbox.get_parameters(coordinate_system=box_utils.CRS_S)
+                label['clf'] = np.ones((max_objects, 1)) * 8
+                label['c_3d'] = np.zeros((max_objects, 3))
+                label['extent'] = np.zeros((max_objects, 3))
+                label['bbox_3d'] = np.zeros((max_objects, 8, 3))
+                label['bbox_2d'] = np.zeros((max_objects, 4))
+                label['c_2d'] = np.zeros((max_objects, 2))
+                label['ri'] = np.zeros((max_objects, 1))
+                label['rj'] = np.zeros((max_objects, 1))
+                for idx, obj in enumerate(label_dict['objects'][:max_objects]):
+                    bbox = box_utils.Box3dImageTransform(camera)
+                    bbox.initialize_box(center=obj['3d']['center'],
+                                        quaternion=obj['3d']['rotation'],
+                                        size=obj['3d']['dimensions'])
+
+                    _, center_3d_cam, quaternion = bbox.get_parameters(coordinate_system=box_utils.CRS_S)
 
 
-                label['clf'][idx, 0] = CLASS_MAP[obj['label']]
-                label['c_3d'][idx, :] = center_3d_cam
-                label['extent'][idx, :] = [obj['3d']['dimensions'][2],  # height
-                                           obj['3d']['dimensions'][1],  # width
-                                           obj['3d']['dimensions'][0]]  # length
+                    label['clf'][idx, 0] = CLASS_MAP[obj['label']]
+                    label['c_3d'][idx, :] = center_3d_cam
+                    label['extent'][idx, :] = [obj['3d']['dimensions'][2],  # height
+                                               obj['3d']['dimensions'][1],  # width
+                                               obj['3d']['dimensions'][0]]  # length
 
-                vertices = bbox.get_vertices(coordinate_system=box_utils.CRS_S)
+                    vertices = bbox.get_vertices(coordinate_system=box_utils.CRS_S)
 
-                for idx_vertice, loc in enumerate(bbox.loc):
-                    label['bbox_3d'][idx, idx_vertice, :] = vertices[loc]
-                """
-                    print(np.concatenate([vertices[loc], [1]], axis=0))
+                    for idx_vertice, loc in enumerate(bbox.loc):
+                        label['bbox_3d'][idx, idx_vertice, :] = vertices[loc]
+                    """
+                        print(np.concatenate([vertices[loc], [1]], axis=0))
+    
+                        center = np.matmul(K_matrix, np.concatenate([vertices[loc], [1]]))
+                        center = center[:2] / center[2]
+    
+                        img_arr[int(center[1]), int(center[0]), :] = (255, 255, 255)
+                    cv.imshow("View0", img_arr)
+                    cv.waitKey(0)
+                    exit()"""
 
-                    center = np.matmul(K_matrix, np.concatenate([vertices[loc], [1]]))
-                    center = center[:2] / center[2]
+                    bbox_2d_xy_hw = obj['2d']['amodal']
 
-                    img_arr[int(center[1]), int(center[0]), :] = (255, 255, 255)
-                cv.imshow("View0", img_arr)
-                cv.waitKey(0)
-                exit()"""
+                    bbox_2d_xy_xy = [bbox_2d_xy_hw[0],  # x_1
+                                     bbox_2d_xy_hw[1],  # y_1
+                                     bbox_2d_xy_hw[0]+bbox_2d_xy_hw[2],  # x_1 + width
+                                     bbox_2d_xy_hw[1]+bbox_2d_xy_hw[3]]  # y_1 + height
 
-                bbox_2d_xy_hw = obj['2d']['amodal']
+                    label['c_2d'][idx, :] = [(bbox_2d_xy_xy[0]+((bbox_2d_xy_xy[2]-bbox_2d_xy_xy[0])/2.))/orig_img_size[1],
+                                             (bbox_2d_xy_xy[1]+((bbox_2d_xy_xy[3]-bbox_2d_xy_xy[1])/2.))/orig_img_size[0]]
 
-                bbox_2d_xy_xy = [bbox_2d_xy_hw[0],  # x_1
-                                 bbox_2d_xy_hw[1],  # y_1
-                                 bbox_2d_xy_hw[0]+bbox_2d_xy_hw[2],  # x_1 + width
-                                 bbox_2d_xy_hw[1]+bbox_2d_xy_hw[3]]  # y_1 + height
+                    bbox_2d_xy_xy = np.array([np.clip(bbox_2d_xy_xy[0]/orig_img_size[1], 0, 1),
+                                              np.clip(bbox_2d_xy_xy[1]/orig_img_size[0], 0, 1),
+                                              np.clip(bbox_2d_xy_xy[2]/orig_img_size[1], 0, 1),
+                                              np.clip(bbox_2d_xy_xy[3]/orig_img_size[0], 0, 1)])
 
-                label['c_2d'][idx, :] = [(bbox_2d_xy_xy[0]+((bbox_2d_xy_xy[2]-bbox_2d_xy_xy[0])/2.))/orig_img_size[1],
-                                         (bbox_2d_xy_xy[1]+((bbox_2d_xy_xy[3]-bbox_2d_xy_xy[1])/2.))/orig_img_size[0]]
+                    label['bbox_2d'][idx, :] = bbox_2d_xy_xy
 
-                bbox_2d_xy_xy = np.array([np.clip(bbox_2d_xy_xy[0]/orig_img_size[1], 0, 1),
-                                          np.clip(bbox_2d_xy_xy[1]/orig_img_size[0], 0, 1),
-                                          np.clip(bbox_2d_xy_xy[2]/orig_img_size[1], 0, 1),
-                                          np.clip(bbox_2d_xy_xy[3]/orig_img_size[0], 0, 1)])
+                    yaw, pitch, roll = pyquaternion.Quaternion(quaternion).yaw_pitch_roll
+                    label['ri'][idx, 0] = np.cos(pitch)
+                    label['rj'][idx, 0] = np.sin(pitch)
 
-                label['bbox_2d'][idx, :] = bbox_2d_xy_xy
+                scan = np.ones((5, 3))  # dummy points
 
-                yaw, pitch, roll = pyquaternion.Quaternion(quaternion).yaw_pitch_roll
-                label['ri'][idx, 0] = np.cos(pitch)
-                label['rj'][idx, 0] = np.sin(pitch)
+                label = kitti_utils.remove_dontcare(label)
 
-            scan = np.ones((5, 3))  # dummy points
+                tf_example = create_example(img, scan, label)
 
-            label = kitti_utils.remove_dontcare(label)
-
-            tf_example = create_example(img, scan, label)
-
-            if scene_id % 5 != 0:
-                train_writer.write(tf_example.SerializeToString())
-            else:
-                val_writer.write(tf_example.SerializeToString())
+                if dataset == 'train':
+                    train_writer.write(tf_example.SerializeToString())
+                elif dataset == 'val':
+                    val_writer.write(tf_example.SerializeToString())
 
 
 if __name__ == '__main__':
     cfg = {
         'in_dir': '../data',
-        'dataset': 'train',
+        'dataset': ['train', 'val'],
         'out_train': '../data/tfrecords/cityscapes_train.tfrecord',
         'out_val': '../data/tfrecords/cityscapes_val.tfrecord',
         'n_scenes': -1,
